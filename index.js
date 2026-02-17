fe830bba8a44772cd9b20bb44f753610c71b0e43
IanScottDev <ianscottbizz@gmail.com>
2026-02-17 16:55:14 -0500

    Function Contact.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function contact(event) {
+    event.preventDefault();
+    emailjs
+        sendForm(
+            'service_b8z6qxb',
+            'template_ejvjbnq',
+            event.target,
+            'QGRWz4iBJkHnakbDK'
+        ).then(() => {
+            console.log('This worked')
+        })
+
+}

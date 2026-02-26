817e4bd925fe23513a9ca2bd4002e3bf920f1d0d
IanScottDev <ianscottbizz@gmail.com>
2026-02-25 19:34:58 -0500

    Fixed classList.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('modal__overlay--loading')
-    const success = document.querySelector('modal__overlay--success')
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
     loading.classList += " modal__overlay--visible"
     emailjs
         .sendForm(
